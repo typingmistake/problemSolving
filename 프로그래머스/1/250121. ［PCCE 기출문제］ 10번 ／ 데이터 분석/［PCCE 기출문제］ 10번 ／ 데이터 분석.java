9febf9630e208a0303e4870aa263bd751f506f9a
sunbin <102957984+typingmistake@users.noreply.github.com>
2025-03-18 20:31:33 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 7.54 ms, Memory: 90.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    static Map<String, Integer> map = Map.of("code", 0, "date", 1, "maximum", 2, "remain", 3);
+
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+
+        int[][] answer = Arrays.stream(data).filter((arr) -> arr[map.get(ext)] < val_ext).sorted((a, b) -> {
+            return Integer.compare(a[map.get(sort_by)], b[map.get(sort_by)]);
+        }).toArray(int[][]::new);
+
+        return answer;
+
+    }
+}
