324141abbecce9e9e89d3dd455ad721051b316e5
sunbin <102957984+typingmistake@users.noreply.github.com>
2025-03-27 23:28:25 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 3.78 ms, Memory: 92.7 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,18 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        int[] answer = new int[s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (map.get(c) != null) {
-                answer[i] = i - map.get(c);
-            } else {
-                answer[i] = -1;
-            }
-            map.put(c, i);
-        }
+        int[] answer = {};
         return answer;
     }
 }
