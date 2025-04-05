dc4b04b03d76d35c51abc380ec28aa149549f702
sunbin <102957984+typingmistake@users.noreply.github.com>
2025-04-06 02:21:19 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 18.10 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long solution(int r1, int r2) {
+        long answer = r2 - r1;
+
+        // sqrt(r2^2 - x^2) <= y <= sqrt(r1^2 - x^2)
+        for (int x = 1; x <= r2 - 1; x++) {
+            // y2 = sqrt(r2^2 - x^2)
+            long y2 = (long) Math.sqrt((long) r2 * r2 - (long) x * x);
+            // y1 = sqrt(r1^2 - x^2)
+            long y1 = (long) Math.sqrt((long) r1 * r1 - (long) x * x) == Math.sqrt((long) r1 * r1 - (long) x * x)
+                    ? (long) Math.sqrt((long) r1 * r1 - (long) x * x) - 1
+                    : (long) Math.sqrt((long) r1 * r1 - (long) x * x);
+            // y2 - y1
+            answer += (y2 - y1);
+        }
+        return answer * 4;
+    }
+}
