4d1a59e0dd09944d0b79ee7faab7a71015903402
sunbin <102957984+typingmistake@users.noreply.github.com>
2025-04-09 00:56:35 +0900

    [level 2] Title: 광물 캐기, Time: 4.23 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/README.md
+++ 프로그래머스/2/172927. 광물 캐기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 광물 캐기 - 172927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927#qna) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 09일 00:55:46
+2025년 04월 09일 00:56:33
 
 ### 문제 설명
 
--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,71 +1,51 @@
 import java.util.*;
 
 class Solution {
-    static int[][] table = {
-            { 1, 1, 1 }, { 5, 1, 1 }, { 25, 5, 1 }
+    static Map<String, Integer> map = new HashMap<>() {
+        {
+            put("diamond", 0);
+            put("iron", 1);
+            put("stone", 2);
+        }
     };
 
     public int solution(int[] picks, String[] minerals) {
         int answer = 0;
-        int tot = picks[0] + picks[1] + picks[2];
-        int M = Math.min(tot * 5, minerals.length);
+        int n = minerals.length;
+        int[] mines = new int[3];
+        int[] use = new int[3];
+        for (int i = 0; i < 3; i++) {
+            use[i] = picks[i] * 5;
+        }
 
-        int[] array = new int[M];
-        for (int i = 0; i < M; i++) {
-            if (minerals[i].equals("diamond")) {
-                array[i] = 100;
-            } else if (minerals[i].equals("iron")) {
-                array[i] = 10;
-            } else {
-                array[i] = 1;
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int i = 0; i < n; i++) {
+            String mineral = minerals[i];
+            if (map.containsKey(mineral)) {
+                mines[map.get(mineral)]++;
             }
         }
 
-        int[][] sorted = sort(array);
-        for (int[] arr : sorted) {
-            if (picks[0] > 0) {
-                answer += work(0, arr);
-                picks[0]--;
-            } else if (picks[1] > 0) {
-                answer += work(1, arr);
-                picks[1]--;
-            } else if (picks[2] > 0) {
-                answer += work(2, arr);
-                picks[2]--;
-            } else {
-                break;
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
             }
         }
-        return answer;
-    }
 
-    public static int[][] sort(int[] arr) {
-        int size = arr.length % 5 == 0 ? arr.length / 5 : arr.length / 5 + 1;
-        int[][] result = new int[size][5];
-        for (int i = 0; i < size; i++) {
-            int start = i * 5;
-            int end = Math.min(start + 5, arr.length);
-            for (int j = 0; j < end-start; j++){
-                result[i][j] = arr[start+j];
+        dp[0][0] = 0;
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                if (dp[i][j] == Integer.MAX_VALUE)
+                    continue;
+                if (i < n && j < n) {
+                    dp[i + 1][j] = Math.min(dp[i + 1][j], dp[i][j] + mines[0]);
+                    dp[i][j + 1] = Math.min(dp[i][j + 1], dp[i][j] + mines[1]);
+                    dp[i + 1][j + 1] = Math.min(dp[i + 1][j + 1], dp[i][j] + mines[2]);
+                }
             }
         }
-        int[][] res = Arrays.stream(result)
-                .sorted((a, b) -> Arrays.stream(b).sum() - Arrays.stream(a).sum())
-                .toArray(int[][]::new);
-        return res;
-    }
 
-    public static int work(int pick, int[] arr) {
-        int res = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 100) {
-                res += table[pick][0];
-            } else if (arr[i] == 10) {
-                res += table[pick][1];
-            } else if (arr[i] == 1) {
-                res += table[pick][2];
-            }
-        }
-        return res;
+        return dp[n][n];
     }
 }
