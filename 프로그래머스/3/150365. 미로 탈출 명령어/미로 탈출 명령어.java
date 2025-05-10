55aca474eb0cc4bc84baca3032478000c974e694
sunbin <102957984+typingmistake@users.noreply.github.com>
2025-05-10 17:43:03 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 11.62 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,6 +1,53 @@
 class Solution {
+    int n, m, r, c, k;
+    int[] dr = { 1, 0, 0, -1 };
+    int[] dc = { 0, -1, 1, 0 };
+    String[] dir = { "d", "l", "r", "u" };
+    
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        String answer = "";
-        return answer;
+        this.n = n;
+        this.m = m;
+        this.r = r;
+        this.c = c;
+        this.k = k;
+        String answer = dfs("", x, y, 0);
+        return answer.equals("") ? "impossible" : answer;
+    }
+    
+    public String dfs(String path, int curr_r, int curr_c, int count) {
+        if (curr_r == r && curr_c == c && count == k) {
+            return path;
+        }
+        
+        // 가지 치기 1
+        if(count == k){
+            return "";
+        }
+        
+        // 가지 치기 2
+        if (Math.abs(curr_r - r) + Math.abs(curr_c - c) > k - count) {
+            return "";
+        }
+        
+        // 가지 치기 3
+        if ((Math.abs(curr_r - r) + Math.abs(curr_c - c)) % 2 != (k - count) % 2) {
+            return "";
+        }
+        
+        for (int i = 0; i < 4; i++) {
+            int next_r = curr_r + dr[i];
+            int next_c = curr_c + dc[i];
+
+            if (next_r < 1 || next_r > n || next_c < 1 || next_c > m) {
+                continue;
+            }
+            
+            String result = dfs(path + dir[i], next_r, next_c, count + 1);
+            if (!result.equals("")) {
+                return result;
+            }
+        }
+        
+        return "";
     }
 }
