64de562ae29246698294cd06b30291bc8754c08c
sunbin <102957984+typingmistake@users.noreply.github.com>
2025-05-16 12:39:55 +0900

    [level 3] Title: 양과 늑대, Time: 9.59 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -29,44 +29,36 @@ class Solution {
     }
 
     public int dfs(Node head, int[] info) {
-        int maxSheep = 0;
+        int ans = 0;
+
         // 노드, 양 수, 늑대 수, 방문 가능한 노드 리스트
         Deque<Object[]> stack = new ArrayDeque<>();
 
-        // 루트 노드는 항상 양
         List<Node> initialPath = new ArrayList<>();
         // 루트 노드의 자식들을 방문 가능한 노드에 추가
         for (Node child : head.children) {
             initialPath.add(child);
         }
 
-        stack.push(new Object[] { head, 1, 0, initialPath });
+        stack.push(new Object[] { new int[] { 1, 0 }, initialPath });
 
         while (!stack.isEmpty()) {
-            Object[] current = stack.pop();
-            Node node = (Node) current[0];
-            int sheep = (int) current[1];
-            int wolves = (int) current[2];
-            List<Node> availableNodes = (List<Node>) current[3];
+            Object[] curr = stack.removeLast();
+            int[] count = (int[]) curr[0];
+            List<Node> availableNodes = (List<Node>) curr[1];
 
             // 양의 최대 수 갱신
-            maxSheep = Math.max(maxSheep, sheep);
+            ans = Math.max(ans, count[0]);
 
             // 다음 방문 가능한 모든 노드에 대해
             for (int i = 0; i < availableNodes.size(); i++) {
                 Node nextNode = availableNodes.get(i);
-                int nextSheep = sheep;
-                int nextWolves = wolves;
 
-                // 양인지 늑대인지 확인
-                if (nextNode.val == 0) {
-                    nextSheep++;
-                } else {
-                    nextWolves++;
-                }
+                int[] nextCount = new int[] { count[0], count[1] }; // 양, 늑대 수
+                nextCount[nextNode.val]++;
 
                 // 양이 늑대보다 많아야 방문 가능
-                if (nextSheep > nextWolves) {
+                if (nextCount[0] > nextCount[1]) {
                     // 다음 방문 가능한 노드 목록 갱신
                     List<Node> newAvailable = new ArrayList<>(availableNodes);
                     newAvailable.remove(i); // 현재 방문한 노드 제거
@@ -76,11 +68,11 @@ class Solution {
                         newAvailable.add(child);
                     }
 
-                    stack.push(new Object[] { nextNode, nextSheep, nextWolves, newAvailable });
+                    stack.push(new Object[] { nextCount, newAvailable });
                 }
             }
         }
 
-        return maxSheep;
+        return ans;
     }
 }
