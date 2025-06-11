5e21e5cc3802f79eaa14b260d4d141caafc9e774
sunbin <102957984+typingmistake@users.noreply.github.com>
2025-06-11 20:15:46 +0900

    [level 3] Title: 최고의 집합, Time: 0.29 ms, Memory: 55.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int n, int s) {
+        int[] answer = new int[n];
+        int m = s/n;
+        int p = s%n;
+        if(m == 0)
+            return new int[]{-1};
+        
+        for(int i = n-1; i >= 0; i--){
+            if(p-- > 0){
+                answer[i] = m+1;
+                continue;
+            }
+            answer[i] = m;
+        }
+        return answer;
+    }
+}
