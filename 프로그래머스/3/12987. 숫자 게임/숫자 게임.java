4b9830d8b6d5b5c69f03fdd9d0c544c7ca33e157
sunbin <102957984+typingmistake@users.noreply.github.com>
2025-06-21 00:37:10 +0900

    [level 3] Title: 숫자 게임, Time: 105.10 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -1,23 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] A, int[] B) {
-        int answer = 0;
-        int idxA = 0;
-        int idxB = 0;
-        
-        Arrays.sort(A);
-        Arrays.sort(B);
-        
-        while(idxA < A.length && idxB < B.length){
-            if(A[idxA] >= B[idxB]){
-                idxB++;
-                continue;
-            }
-            answer++;
-            idxA++;
-            idxB++;
-        }
+        int answer = -1;
         return answer;
     }
 }
