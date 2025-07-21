635fd48bc8e0ae4d74bc971e75b2f234902f2d19
sunbin <102957984+typingmistake@users.noreply.github.com>
2025-07-22 00:56:09 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.34 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,19 @@
+class Solution {
+    int MOD = 1234567;
+    public long solution(int n) {
+        long answer = 0;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        
+        for(int i = 0; i < n; i++){
+            int[] nexts = new int[]{i+1,i+2};
+            
+            for(int next : nexts){
+                if(next <= n)
+                    dp[next] = (dp[next] + dp[i])%MOD;
+            }
+        }
+        
+        return dp[n];
+    }
+}
