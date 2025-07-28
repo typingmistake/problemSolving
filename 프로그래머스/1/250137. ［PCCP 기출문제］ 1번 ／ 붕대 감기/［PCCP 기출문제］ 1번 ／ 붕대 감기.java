ea6ac5a0a03563b34143eade0ea02117a57c82c7
sunbin <102957984+typingmistake@users.noreply.github.com>
2025-07-28 23:00:00 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.05 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,33 +1,25 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-        int t = bandage[0];
-        int x = bandage[1];
-        int y = bandage[2];
-        int c = 0; // 연속 회복 횟수
-        int m = 0; // 몇번째 몬스터 공격인지
-        int h = health;
+        int hp = health;
+        int prev = 0; // 이전 공격 시간
 
-        for (int i = 1; i <= attacks[attacks.length - 1][0]; i++) {
-            if (i == attacks[m][0]) {
-                h -= attacks[m++][1];
-                c = 0;
-                if (h <= 0) {
-                    return -1; // 죽음
-                }
-                continue;
+        for(int[] attack : attacks){
+            // 회복
+            int t = attack[0] - prev - 1;
+            prev = attack[0];
+            
+            int add =  t*bandage[1] + bandage[2] * (t / bandage[0]);
+            hp = Math.min(hp+add, health);
+            
+            // 공격
+            hp -= attack[1];
+            
+            // 체크
+            if(hp <= 0){
+                return -1;
             }
-
-            h += x;
-            if (++c == t) {
-                h += y;
-                c = 0;
-            }
-
-            h = Math.min(h, health); // 최대 체력은 health
         }
-
-        return h;
+        
+        return hp;
     }
 }
