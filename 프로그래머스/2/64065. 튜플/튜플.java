80e6c664f011d2cd976b449bade7ac4a3b903505
sunbin <102957984+typingmistake@users.noreply.github.com>
2025-08-08 12:55:56 +0900

    [level 2] Title: 튜플, Time: 66.30 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,33 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(String s) {
-        String[] parts = s.substring(2, s.length() - 2).split("\\},\\{");
-        List<int[]> list = new ArrayList<>();
-
-        for (String part : parts) {
-            String[] tokens = part.split(",");
-            int[] arr = new int[tokens.length];
-            for (int i = 0; i < tokens.length; i++) {
-                arr[i] = Integer.parseInt(tokens[i]);
-            }
-            list.add(arr);
-        }
-
-        list.sort((a, b) -> a.length - b.length); // 길이 짧은 기준으로 정렬
-        Set<Integer> set = new HashSet<>();
-        int[] answer = new int[list.size()];
-
-        int idx = 0;
-        for (int[] arr : list) {
-            for (int num : arr) {
-                if (!set.contains(num)) {
-                    set.add(num);
-                    answer[idx++] = num;
-                }
-            }
-        }
-
+        int[] answer = {};
         return answer;
     }
 }
