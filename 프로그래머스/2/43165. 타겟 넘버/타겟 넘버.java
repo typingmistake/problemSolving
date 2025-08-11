0f819235e57294b7572389d091c50a6207d4d7e2
sunbin <102957984+typingmistake@users.noreply.github.com>
2025-08-11 23:23:06 +0900

    [level 2] Title: 타겟 넘버, Time: 8.67 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int[] numbers, int target) {
-        int answer = 0;
         return dfs(numbers, 0, 0, target);
     }
+    
     public int dfs(int[] numbers, int idx, int curr, int target){
         if(idx == numbers.length && curr == target)
             return 1;
@@ -10,6 +10,7 @@ class Solution {
         if(idx >= numbers.length)
             return 0;
         
-        return dfs(numbers, idx+1, curr-numbers[idx], target) + dfs(numbers, idx+1, curr+numbers[idx], target);
+        return dfs(numbers, idx+1, curr-numbers[idx], target) 
+            + dfs(numbers, idx+1, curr+numbers[idx], target);
     }
 }
