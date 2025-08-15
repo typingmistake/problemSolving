14bb188317f0be0b07e251df7e477b7e7e53347e
sunbin <102957984+typingmistake@users.noreply.github.com>
2025-08-16 05:21:56 +0900

    [level 2] Title: [3차] n진수 게임, Time: 16.86 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,37 +1,6 @@
-import java.util.*;
-
 class Solution {
-    static String digits = "0123456789ABCDEF";
     public String solution(int n, int t, int m, int p) {
         String answer = "";
-        int cnt = 0;
-        int num = 0;
-        p--;
-        List<Character> list = new ArrayList<>();
-        while (cnt <= m * t) {
-            for (char c : convertBase(num++, n).toCharArray()) {
-                list.add(c);
-                cnt++;
-            }
-        }
-        cnt = 0;
-        while (cnt++ < t) {
-            answer += list.get(p);
-            p += m;
-        }
         return answer;
-    }
-
-    public static String convertBase(int num, int m) {
-        if (num == 0)
-            return "0";
-
-        StringBuilder sb = new StringBuilder();
-        while (num > 0) {
-            sb.append(digits.charAt(num%m));
-            num /= m;
-        }
-
-        return sb.reverse().toString();
     }
 }
