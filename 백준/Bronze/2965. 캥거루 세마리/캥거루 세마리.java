839919ab2c97cff67a3f769c52c5ebee6b56b8d9
sunbin <102957984+typingmistake@users.noreply.github.com>
2025-12-01 00:34:24 +0900

    [Bronze III] Title: 캥거루 세마리, Time: 108 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/2965. 캥거루 세마리/캥거루 세마리.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+        int C = Integer.parseInt(input[2]);
+
+        System.out.println(Math.max((B-A-1), (C-B-1)));
+    }
+}
