e0afcae039c11c17bc1388ba18a880f138eec85a
sunbin <102957984+typingmistake@users.noreply.github.com>
2025-12-22 23:45:22 +0900

    [Gold III] Title: 크게 만들기, Time: 392 ms, Memory: 36032 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.java
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -18,7 +18,7 @@ public class Main {
         Stack<Integer> stack = new Stack<>();
         for(int i = 0; i < N; i++){
             int curr = nums[i];
-            int size = i-K+1;
+            int size = i-K+1; // 최소한 이만큼은 남겨둬야 함.
 
             while(!stack.isEmpty() && stack.size() >= size && stack.peek() < curr){
                 stack.pop();
@@ -27,6 +27,7 @@ public class Main {
             stack.push(curr);
         }
 
+        // K개 제거하고 선택
         StringBuilder ans = new StringBuilder();
         for(int i = 0; i < N - K; i++){
             ans.append(stack.get(i));
