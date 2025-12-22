58b83a6d8ba793dc7d4f3843f4457b7da8b378bc
sunbin <102957984+typingmistake@users.noreply.github.com>
2025-12-22 23:37:47 +0900

    [Gold III] Title: 크게 만들기, Time: 404 ms, Memory: 35108 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+
+        String numStr = br.readLine().strip();
+        int[] nums = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = numStr.charAt(i) - '0';
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < N; i++){
+            int curr = nums[i];
+            int size = i-K+1;
+
+            while(!stack.isEmpty() && stack.size() >= size && stack.peek() < curr){
+                stack.pop();
+            }
+
+            stack.push(curr);
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for(int i = 0; i < N - K; i++){
+            ans.append(stack.get(i));
+        }
+
+        System.out.println(ans);
+    }
+}
