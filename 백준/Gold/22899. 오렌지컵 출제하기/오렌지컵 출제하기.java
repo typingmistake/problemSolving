0fd7ff2a7eaf63d8de0bb790b0caff40cb3b7606
sunbin <102957984+typingmistake@users.noreply.github.com>
2026-01-25 04:01:44 +0900

    [Gold II] Title: 오렌지컵 출제하기, Time: 788 ms, Memory: 76480 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/22899. 오렌지컵 출제하기/오렌지컵 출제하기.java
+++ 백준/Gold/22899. 오렌지컵 출제하기/오렌지컵 출제하기.java
@@ -7,48 +7,48 @@ public class Main {
         String[] input = br.readLine().split(" ");
         int N = Integer.parseInt(input[0]);
         int K = Integer.parseInt(input[1]);
-        int[][] arr = new int[N][2];
+
         String[] idx = br.readLine().split(" ");
         String[] time = br.readLine().split(" ");
-        
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
         for (int i = 0; i < N; i++) {
-            arr[i][0] = Integer.parseInt(idx[i]);
-            arr[i][1] = Integer.parseInt(time[i]);
-        }
-        
-        Arrays.sort(arr, (a, b) -> {
-            if (a[0] == b[0]) return a[1] - b[1];
-            return a[0] - b[0];
-        });
-        
-        Map<Integer, Queue<Integer>> map = new LinkedHashMap<>();
-        for (int i = 0; i < N; i++) {
-            int id = arr[i][0];
-            int t = arr[i][1];
-            map.putIfAbsent(id, new ArrayDeque<>());
+            int id = Integer.parseInt(idx[i]);
+            int t = Integer.parseInt(time[i]);
+
+            map.putIfAbsent(id, new ArrayList<>());
             map.get(id).add(t);
         }
-        
+
+        // 레벨별 그룹화
+        List<List<Integer>> levels = new ArrayList<>();
+        // 최대 레벨 N
+        for (int i = 0; i < N; i++) levels.add(new ArrayList<>());
+
+        for (List<Integer> list : map.values()) {
+            Collections.sort(list);
+            for (int i = 0; i < list.size(); i++) {
+                levels.get(i).add(list.get(i));
+            }
+        }
+
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
         long result = 0;
         StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < N; i++) {
-            for (int key : map.keySet()) {
-                Queue<Integer> q = map.get(key);
-                if (q.isEmpty()) continue;
-                int t = q.poll();
-                
-                pq.add(t);
-                result += t;
-                
-                if (pq.size() > K) {
-                    result -= pq.poll();
+
+        for (int L = 1; L <= N; L++) {
+            for (int t : levels.get(L - 1)) {
+                if (pq.size() < K || pq.peek() > t) {
+                    pq.add(t);
+                    result += t;
+                    if (pq.size() > K) {
+                        result -= pq.poll();
+                    }
                 }
             }
             sb.append(pq.size() >= K ? result : -1).append(" ");
         }
-        
+
         System.out.print(sb);
     }
 }
